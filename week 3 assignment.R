0f21ea533ec2c623e12ec67e8768a28633c26588
chauncey922 <chauncey922@foxmail.com>
2016-11-20 19:36:45 +0800

    week 3 assignemnt

--- week 3 assignment.R
+++ week 3 assignment.R
@@ -1,3 +1,10 @@
+## The following functions cashe the inverse of a matrix
+
+## This function creates a special "matrix" object that:
+### 1) set the value of the matrix 
+### 2) get the value of the matrix
+### 3) set the value of the inversed matrix &
+### 4) get the value of the inversed matrix
 makeCacheMatrix <- function(x = numeric()){
   m<-NULL
   set <- function(y){
@@ -9,6 +16,12 @@ makeCacheMatrix <- function(x = numeric()){
   getsolve <- function() m
   list(set=set, get= get, setsolve = setsolve, getsolve = getsolve)
 }
+
+
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated, then the cachesolve should retrieve the inverse 
+## from the cache. Otherwise, it calculates the inverse of the matrix & sets the value of the 
+## inversed matrix in the cache via the set_inv function
 cacheSolve <- function(x, ...){
   m <- x$getmsolve()
   if(!is.null(m)){
